71010531e3581ceba7ee3e7a05e98e53d20dc58c
Nikolaos Sarris <nikossarris29@gmail.com>
2026-09-24 15:14:14 +0200

    Add method for printing circle info

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ namespace OOP;
 
 class Program
 {
+    static void Main(string[] args)
+    {
+        PrintCircleInfo(5);
+        PrintCircleInfo(6);
 
+    }
+
+    static void PrintCircleInfo(double radius)
+    {
+        Circle circle = new Circle(radius);
+
+        Console.WriteLine($"Circle info: \nRadius: {radius} \nArea: {circle.GetArea()}.");
+        Console.WriteLine($"Diameter: {circle.GetCircumference()}.");
+        Console.WriteLine($"Volume: {circle.GetVolume()}.\n");
+    }
 
 }
