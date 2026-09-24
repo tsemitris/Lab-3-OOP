320a70e5569d4a686d7dda04161c07b102bc44f6
Nikolaos Sarris <nikossarris29@gmail.com>
2026-09-24 15:15:08 +0200

    Create Triangle class & variables

---
+++ Triangle.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace OOP;
+
+public class Triangle
+{
+    double _base;
+    double _height;
+    double pi = Math.PI;
+
+    public double Base
+    {
+        get { return _base; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Base can't be negative");
+            }
+
+            _base = value;
+        }
+    }
+    public double Height
+    {
+        get { return _height; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Height can't be negative");
+            }
+
+            _height = value;
+        }
+    }
+}
