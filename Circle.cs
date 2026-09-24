cabdb4cb2b5d9a8cda7b87d2388c2bef72a84f50
Nikolaos Sarris <nikossarris29@gmail.com>
2026-09-24 15:11:50 +0200

    Create Circle class and variables

---
+++ Circle.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace OOP;
+
+public class Circle
+{
+    double _radius;
+
+    public double Radius
+    {
+        get { return _radius; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Radius can't be negative");
+            }
+
+            _radius = value;
+        }
+    }
+}
